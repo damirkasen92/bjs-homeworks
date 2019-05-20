48d17169e427074666f1f5b9530b2565614afc07
damirkasen92 <damir.kasen92@gmail.com>
2019-05-20 12:38:39 +0600

    Скрипты в одном файле

--- 1.1/main.js
+++ 1.1/main.js
@@ -5,3 +5,16 @@ let physics = 4;
 let averageScore = (algebra + geography + physics) / 3;
 
 console.log(averageScore);
+
+let name = 'Дамир';
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
--- 1.2/main.js
+++
@@ -1,4 +0,0 @@
-let name = 'Дамир';
-let message = 'Привет, мир! Меня зовут ' + name;
-
-console.log(message);
