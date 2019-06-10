3f4835c78b85432e584399a3c5e5470cbf04503e
damirkasen92 <damir.kasen92@gmail.com>
2019-06-10 12:41:31 +0600

    Задание array-proto-hocs

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,56 +1,58 @@
-const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((a1, a2) => a1 === arr2[a2]);
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((el, index) => el === arr2[index]);
 
 const memorize = (fn, limit) => {
   let results = [];
-  console.log(results);
-  
-  return function (fn) {
-    let obj = {
-      args: Array.from(arguments),
-      result: sum(...arguments),
-    };
-    
-    if (results.length + 1 > limit) {
-      results.shift();
-    } 
-    
-    const same = results.map(item => item.args);
-    
-    for (let i = 0; i < same.length; i++) {
-      if (compareArrays(obj.args, same[i]) === true) {
-        console.log(`Нашел совпадение: ${same[i]}`);
 
-        return this.result;
-      } 
+  return function (...arguments) {
+    let obj = {};
+    obj.args = Array.from(arguments);
+    
+    function same(res) {
+      return res.args.toString() === obj.args.toString();
     }
-    
-    results.push(obj);
 
-    console.log(`Функция вызвана не из памяти`);
-    console.log(results);
+    const findSame = results.find(same);
 
-    return obj.result;
+    if (findSame !== undefined && compareArrays(obj.args, findSame.args) === true) {
+      console.log(`Нашел совпадение: ${findSame.args}`);
+      console.log(`Результат берётся из памяти: ${findSame.result}`);
+
+      return findSame.result;
+    } else {
+      console.log(`Совпадений не найдено`);
+
+      obj.result = fn(arguments);
+      results.push(obj);
+
+      if (results.length > limit) results.shift();
+
+      return obj.result;
+    }
   }
-  
-  console.log(`Результат берётся из памяти`);
 }
 
-const sum = (...arguments) => {
+const sum = (arguments) => {
   let sumOfArgs = 0;
+  let args = Array.from(arguments);
   
-  for (let i = 0; i < arguments.length; i++) {
-    sumOfArgs += arguments[i];
-  }
+  args.forEach(function(el) {
+    sumOfArgs += el;
+  });
   
+  console.log(`Функция вызвана не из памяти ${sumOfArgs}`);
+
   return sumOfArgs;
 }
 
-const mSum = memorize(sum, 4);
+const mSum = memorize(sum, 5);
 
 mSum(3, 4);
+mSum(2, 8);
 mSum(3, 4);
 mSum(2, 8);
-mSum(2, 8);
 mSum(4, 8);
+mSum(6, 8);
 mSum(5, 12);
+mSum(6, 8);
+mSum(7, 15);
 mSum(6, 8);
